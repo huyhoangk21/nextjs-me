e85ba923c2abbf6def20330c350b60f4b257f4a1
Hoang Le <kuhpro@gmail.com>
2022-03-10 19:53:44 -0600

    fix missing css style for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
             Latte.
           </p>
         </div>
-        <div className='rounded-image h-40 w-40 mx-auto'>
+        <div className='relative rounded-full overflow-hidden h-40 w-40 mx-auto'>
           <Image
             src='https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1085&q=80'
             layout='fill'
